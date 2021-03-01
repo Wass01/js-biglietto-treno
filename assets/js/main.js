43ebb288e137e59785b35257eab77f0d254bbd49
wass01 <maatallawassim01@gmail.com>
2021-03-01 15:12:27 +0100

    inserimento variabili

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+var eta = prompt("Inserisci la tua età:");
+var chilometri = prompt("Inserisci i chilometri che vuoi fare:");
+
+console.log(parseInt(eta));
+console.log(parseFloat(chilometri));
+
+var prezzoBiglietto = chilometri * 0.21;
+console.log(prezzoBiglietto);
