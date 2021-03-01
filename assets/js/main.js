291c6a308dab16fb524d0f5c236ee9b244cd56a2
wass01 <maatallawassim01@gmail.com>
2021-03-01 15:27:30 +0100

    ESERCIZIO TERMINATO

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,18 @@
+// creazione variabili necessarie
 var eta = prompt("Inserisci la tua età:");
 var chilometri = prompt("Inserisci i chilometri che vuoi fare:");
+var prezzoBiglietto;
 
 console.log(parseInt(eta));
 console.log(parseFloat(chilometri));
 
-var prezzoBiglietto = chilometri * 0.21;
-console.log(prezzoBiglietto);
+// condizione per il calcolo
+if(eta < 18){
+  prezzoBiglietto = (chilometri * 0.21) - (chilometri * 0.21) * 0.2;
+} else if(eta > 65){
+  prezzoBiglietto = (chilometri * 0.21) - (chilometri * 0.21) * 0.4;
+} else {
+  prezzoBiglietto = (chilometri * 0.21);
+}
+
+console.log("Il prezzo del biglietto è: " + prezzoBiglietto + "€");
